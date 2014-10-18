60b0cd33b44084780bc7074e43d0cbcbd97c55c0
Joestetz <Joestetz@gmail.com>
2014-10-18 09:51:30 -0400

    initial scaffold

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('nflPickemApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
